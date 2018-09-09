b7da4e7f0d1b8452ab56b15d8a410f9147f38e78
Сергій Когут <sergio.kogut@gmail.com>
2018-09-09 20:27:04 +0300

    begin

--- Autocad/Form1.Designer.cs
+++ Autocad/Form1.Designer.cs
@@ -28,13 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.btnLine = new System.Windows.Forms.Button();
+            this.btnCaption = new System.Windows.Forms.Button();
+            this.btnCircle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtRadius.Location = new System.Drawing.Point(85, 50);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(110, 26);
+            this.txtRadius.TabIndex = 0;
+            // 
+            // btnLine
+            // 
+            this.btnLine.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.btnLine.Location = new System.Drawing.Point(238, 152);
+            this.btnLine.Name = "btnLine";
+            this.btnLine.Size = new System.Drawing.Size(206, 34);
+            this.btnLine.TabIndex = 1;
+            this.btnLine.Text = "Створити лінію";
+            this.btnLine.UseVisualStyleBackColor = true;
+            this.btnLine.Click += new System.EventHandler(this.btnLine_Click);
+            // 
+            // btnCaption
+            // 
+            this.btnCaption.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.btnCaption.Location = new System.Drawing.Point(238, 101);
+            this.btnCaption.Name = "btnCaption";
+            this.btnCaption.Size = new System.Drawing.Size(206, 34);
+            this.btnCaption.TabIndex = 2;
+            this.btnCaption.Text = "Добавити надпис";
+            this.btnCaption.UseVisualStyleBackColor = true;
+            this.btnCaption.Click += new System.EventHandler(this.btnCaption_Click);
+            // 
+            // btnCircle
+            // 
+            this.btnCircle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.btnCircle.Location = new System.Drawing.Point(238, 50);
+            this.btnCircle.Name = "btnCircle";
+            this.btnCircle.Size = new System.Drawing.Size(206, 35);
+            this.btnCircle.TabIndex = 3;
+            this.btnCircle.Text = "Створити коло";
+            this.btnCircle.UseVisualStyleBackColor = true;
+            this.btnCircle.Click += new System.EventHandler(this.btnCircle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(538, 450);
+            this.Controls.Add(this.btnCircle);
+            this.Controls.Add(this.btnCaption);
+            this.Controls.Add(this.btnLine);
+            this.Controls.Add(this.txtRadius);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Autocad Profil";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.Button btnLine;
+        private System.Windows.Forms.Button btnCaption;
+        private System.Windows.Forms.Button btnCircle;
     }
 }
 
--- Autocad/Form1.cs
+++ Autocad/Form1.cs
@@ -1,20 +1,141 @@
-﻿using System;
+﻿using AutoCAD;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace Autocad
 {
     public partial class Form1 : Form
     {
+        // Creating object reference to hold the reference of the circle to be created.
+        private AcadCircle Circle = default(AcadCircle);
+
+        // Creating object reference to hold the reference of running AutoCAD instance.
+        private AcadApplication AcadApp = default(AcadApplication);
+
+
         public Form1()
         {
             InitializeComponent();
+            AcadApp = (AcadApplication)Marshal.GetActiveObject("AutoCAD.Application");
+        }
+
+        private void btnLine_Click(object sender, EventArgs e)
+        {
+            //
+            AcadLine Line= default(AcadLine);
+
+            //  AcadLine acLine = new AcadLine(new AcadPoint(5, 5, 0), new AcadPoint(12, 3, 0));
+            //Line = AcadApp.ActiveDocument.ModelSpace.AddLine(new AcadPoint(5, 5, 0),);
+
+            
+
+        }
+
+        private void btnCaption_Click(object sender, EventArgs e)
+        {
+            // Caption - AcadText object to hold the reference of the AcadText 
+            AcadText Caption = default(AcadText);
+
+            // Syntax for creating Text in AutoCAD
+            // AcadApp.ActiveDocument.ModelSpace.AddText(TextString, InsertionPoint, Height);
+            // 
+            // AcadApp        - Reference to the running instance of AutoCAD.
+            // ActiveDocument - Represents the current working drawing in AutoCAD.
+            // ModelSpace     - Work Area in the current drawing.
+            // AddText        - Method, which adds a Text to the modelspace of the current drawing using the TextString, InsertionPoint and Height.
+            // TextString     - string, which represents the text to be written.
+            // InsertionPoint - 3 Dimensional double array variable holds the insertion point of the circle in the X, Y and Z axis.
+            // Height         - Double variable holds the height of text.
+
+            // Text to be written just below the circle as a caption.
+            string TextString = "CIRCLE";
+
+            // Definfing the insertion point for the circle, in this example, we are going to place the text just below the circle.
+            double[] InsertionPoint = new double[3];
+
+            // Getting Centerpoint of circle we have created earlier to define the insertion point for the Caption.
+            double[] CenterOfCircle = (double[])Circle.Center;
+
+            InsertionPoint[0] = 0;
+            InsertionPoint[1] = 0;
+            InsertionPoint[2] = 0;
+
+            // Defining height of the text
+            double Height = 200;
+
+            // Adding text to the modelspace and getting reference to the text created.
+            Caption = AcadApp.ActiveDocument.ModelSpace.AddText(TextString, InsertionPoint, Height);
+
+            // Changing the text color
+            Caption.color = ACAD_COLOR.acGreen;
+            
+        }
+
+        private void DrawCircle(double StartingXPoint, double StartingYPoint, double Radius)
+
+        {
+            AcadCircle circleObj;
+            double[] centerPoint = new double[3];
+
+            centerPoint[0] = StartingXPoint;
+            centerPoint[1] = StartingYPoint;
+            centerPoint[2] = 0.0;
+            circleObj = AcadApp.ActiveDocument.ModelSpace.AddCircle(centerPoint, Radius);
+
+        }
+
+
+
+        public void Line(double[] Point1, double[] Point2, ACAD_COLOR LineColor)
+        {
+            
+            AcadLine AClineSample = default(AcadLine);
+            AClineSample = AcadApp.ActiveDocument.ModelSpace.AddLine(Point1, Point2);
+            AClineSample.color = LineColor;
+         
+        }
+
+        private void btnCircle_Click(object sender, EventArgs e)
+        {
+            
+
+          //  AcadApp = (AcadApplication)Marshal.GetActiveObject("AutoCAD.Application");
+
+            // Now AcadApp has the reference to the running AutoCAD instance, AcadApp object acts as a port for accessing running instance of AutoCAD.
+
+            // Syntax for creating circle in AutoCAD
+            // AcadApp.ActiveDocument.ModelSpace.AddCircle(CenterOfCircle, RadiusOfCircle);
+            //
+            // AcadApp        - Reference to the running instance of AutoCAD.
+            // ActiveDocument - Represents the current working drawing in AutoCAD.
+            // ModelSpace     - Work Area in the current drawing.
+            // AddCricle      - Method, which adds a circle to the modelspace of the current drawing using the CenterPoint and Radius.
+            // CenterOfCircle - 3 Dimensional double array variable holds the center point of the circle in the X, Y and Z axis.
+            // RadiusOfCircle - Double variable holds the radius of circle.
+
+            // Definfing the center point for the circle, in this example, we are using origin(0,0,0) as the center of circle.
+            double[] CenterOfCircle = new double[3];
+            CenterOfCircle[0] = 0;
+            CenterOfCircle[1] = 0;
+            CenterOfCircle[2] = 0;
+
+            // Defining radius of circle from the user input.
+            double RadiusOfCircle = Convert.ToDouble(txtRadius.Text.Trim());
+
+
+            // Adding Circle to the modelspace and getting reference to the circle created.
+            Circle = AcadApp.ActiveDocument.ModelSpace.AddCircle(CenterOfCircle, RadiusOfCircle);
+
+
         }
     }
 }
